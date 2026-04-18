043b48fe3c5569d707446e8a2ea5fec6860b1c89
qkrwns1478 <qkrwns1478@gmail.com>
2026-04-18 15:28:25 +0900

    add: 데이터셋 추가

--- frontend/src/data/SampleTeams.ts
+++ frontend/src/data/SampleTeams.ts
@@ -1,6 +1,6 @@
 const SAMPLE_TEAMS = {
   team1: `Pikachu @ Light Ball\nAbility: Static\nEVs: 252 SpA / 4 SpD / 252 Spe\nTimid Nature\n- Thunderbolt\n- Quick Attack\n- Iron Tail\n- Volt Tackle\n\nCharizard @ Heavy-Duty Boots\nAbility: Blaze\nEVs: 252 SpA / 4 SpD / 252 Spe\nTimid Nature\n- Flamethrower\n- Air Slash\n- Dragon Pulse\n- Roost`,
-  team2: `Gengar @ Life Orb\nAbility: Cursed Body\nEVs: 252 SpA / 4 SpD / 252 Spe\nTimid Nature\n- Shadow Ball\n- Sludge Bomb\n- Focus Blast\n- Destiny Bond\n\nSnorlax @ Leftovers\nAbility: Thick Fat\nEVs: 252 HP / 4 Atk / 252 SpD\nCareful Nature\n- Body Slam\n- Earthquake\n- Rest\n- Sleep Talk`
+  team2: `Gengar @ Life Orb\nAbility: Cursed Body\nEVs: 252 SpA / 4 SpD / 252 Spe\nTimid Nature\n- Shadow Ball\n- Sludge Bomb\n- Focus Blast\n- Destiny Bond\n\nSnorlax @ Leftovers\nAbility: Thick Fat\nEVs: 252 HP / 4 Atk / 252 SpD\nCareful Nature\n- Body Slam\n- Earthquake\n- Rest\n- Sleep Talk`,
 };
 
 export default SAMPLE_TEAMS;
