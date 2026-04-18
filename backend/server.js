49f2acb25bae0b7d88f24f4df65873b26dbf8fc3
qkrwns1478 <qkrwns1478@gmail.com>
2026-04-19 04:01:15 +0900

    refactor: customDex 분리

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,13 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { BattleStreams, Teams, Dex } from "@pkmn/sim";
+import { applyCustomPokedex } from "./customDex.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-Dex.data.Pokedex["aldina"] = {
-  num: 3384,
-  name: "Aldina",
-  types: ["Ghost"],
-  baseStats: { hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100 },
-  abilities: { 0: "Levitate", H: "Levitate" },
-  weightkg: 3,
-};
-Dex.data.Pokedex["almaria"] = {
-  num: 3385,
-  name: "Almaria",
-  types: ["Ghost"],
-  baseStats: { hp: 100, atk: 125, def: 100, spa: 125, spd: 100, spe: 125 },
-  abilities: { 0: "Levitate", H: "Levitate" },
-  weightkg: 36.2,
-};
-Dex.data.Pokedex["mimikyumane"] = {
-  num: 7778,
-  name: "Mimikyu-Mane",
-  types: ["Ghost", "Fairy"],
-  baseStats: { hp: 55, atk: 55, def: 55, spa: 135, spd: 135, spe: 135 },
-  abilities: { 0: "Disguise", H: "Disguise" },
-  weightkg: 0.7,
-};
+applyCustomPokedex(Dex);
 
 const rooms = new Map();
 const playerRoomMap = new Map();
